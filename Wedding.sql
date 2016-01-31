481f7b35dca8e7613fb2489fc5c02c8bfda2de3e
NuizoroMacAir <ammales.y@gmail.com>
2016-02-01 00:36:32 +0700

    sql file

---
+++ Wedding.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 31, 2016 at 06:36 PM
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Wedding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gift`
+--
+
+CREATE TABLE IF NOT EXISTS `gift` (
+  `id` int(10) NOT NULL,
+  `title` text NOT NULL,
+  `subTitle` text NOT NULL,
+  `amount` int(10) NOT NULL,
+  `path_url` text NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `gift`
+--
+
+INSERT INTO `gift` (`id`, `title`, `subTitle`, `amount`, `path_url`) VALUES
+(1, 'Test(Normal gift)', 'Test(Normal gift) subtitle', 999, ''),
+(2, 'Test(Premium gift)', 'Test(Premium gift) subtitle', 20, ''),
+(3, 'Test(Rare premium gift)', 'Test(Rare premium gift) subtitle', 10, '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gift`
+--
+ALTER TABLE `gift`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `gift`
+--
+ALTER TABLE `gift`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
